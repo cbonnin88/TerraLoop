d298167a775318c69d200f6a4de764827253a50b
Christopher Bonnin <chrisbit.carrington@gmail.com>
2026-02-20 18:58:29 +0000

    updated stg_users

--- models/staging/stg_users.sql
+++ models/staging/stg_users.sql
@@ -4,11 +4,12 @@ WITH raw_data AS (
 
 renamed AS (
     SELECT
-        JSON_VALUE(_airbyte_meta,'$.user_id') AS user_id,
-        JSON_VALUE(_airbyte_meta,'$.user_tier') AS user_tier,
-        JSON_VALUE(_airbyte_meta,'$.region') AS region,
-        CAST(JSON_VALUE(_airbyte_meta,'$.signup_date')AS date) AS signup_date
+        user_id,
+        user_tier,
+        region,
+        SAFE_CAST(signup_date AS date) AS signup_date
     FROM raw_data
+    WHERE user_id IS NOT NULL
 )
 
 SELECT * FROM renamed
